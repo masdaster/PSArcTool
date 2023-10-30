08957a886524b4574fa157c741ffef3d04233d4e
masdaster <118899802+masdaster@users.noreply.github.com>
2023-10-30 09:15:38 +0100

    Upgrade to .NET 6.

--- PSArcTool/Program.cs
+++ PSArcTool/Program.cs
@@ -1,72 +1,56 @@
-﻿
-using System;
+﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 
-namespace PSArcTool
-{
+namespace PSArcTool {
+
     [SuppressMessage("ReSharper", "LocalizableElement")]
-    class Program
-    {
-        static void Main(string[] args)
-        {
+    internal class Program {
+
+        private static void Main(string[] args) {
             var waitForEnter = false;
 
-            try
-            {
-
+            try {
                 Console.WriteLine("PSArcTool");
-                Console.WriteLine("Periander 2016-08-17");
+                Console.WriteLine("masdaster 2023-10-30");
                 Console.WriteLine("I was mucking around with No Man's Sky modding and wanted to make extracting/creating PAK files easier.");
-                foreach (var arg in args)
-                {
+                foreach (var arg in args) {
                     Console.WriteLine(arg);
                 }
 
-
-                //// TESTING 
+                //// TESTING
                 //args = new[]
                 //{
                 //    @"C:\Program Files (x86)\Steam\steamapps\common\No Man's Sky\GAMEDATA\PCBANKS\PSARC.PAK"
                 //    //@"C:\Program Files (x86)\Steam\steamapps\common\No Man's Sky\GAMEDATA\PCBANKS\A.txt",
                 //    //@"C:\Program Files (x86)\Steam\steamapps\common\No Man's Sky\GAMEDATA\PCBANKS\B.txt"
 
-                //    //@"C:\Users\peria\Desktop\New folder\SF\psarc.pak"
-                //    //@"C:\Users\peria\Desktop\New folder\SF\A.txt",
-                //    //@"C:\Users\peria\Desktop\New folder\SF\B.txt"
+                // //@"C:\Users\peria\Desktop\New folder\SF\psarc.pak"
+                // //@"C:\Users\peria\Desktop\New folder\SF\A.txt", //@"C:\Users\peria\Desktop\New folder\SF\B.txt"
 
                 //};
 
                 if (args.Any() &&
-                    args.All(arg => File.Exists(arg) && (arg.EndsWith(".pak", StringComparison.OrdinalIgnoreCase) || arg.EndsWith(".psarc", StringComparison.OrdinalIgnoreCase))))
-                {
-                    foreach (var pakFilePath in args)
-                    {
+                    args.All(arg => File.Exists(arg) && (arg.EndsWith(".pak", StringComparison.OrdinalIgnoreCase) || arg.EndsWith(".psarc", StringComparison.OrdinalIgnoreCase)))) {
+                    foreach (var pakFilePath in args) {
                         Functions.Extract(pakFilePath);
                     }
-                }
-                else if (args.Any() && args.All(arg => File.Exists(arg) || Directory.Exists(arg)))
-                    // A collection of files or folders
-                {
+                } else if (args.Any() && args.All(arg => File.Exists(arg) || Directory.Exists(arg)))
+                  // A collection of files or folders
+                  {
                     Functions.Create(args, string.Empty);
-                }
-                else
-                {
+                } else {
                     waitForEnter = true;
                     Console.WriteLine(
                         "Usage: Pass PAK/PSARC files as arguments to extract (click and drag on to PSArcTool). Pass anything else to create a PAK file.");
                 }
-
-            }
-            catch (Exception exc)
-            {
+            } catch (Exception exc) {
                 waitForEnter = true;
                 Console.WriteLine(exc);
             }
 
-            if (waitForEnter)
-            {
+            if (waitForEnter) {
                 Console.WriteLine("Press ENTER to quit.");
                 Console.ReadLine();
             }
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # PSArcTool
-I was mucking around with PSArc, but got tired of the command line, so wrote a little tool:
-https://github.com/periander/PSArcTool
-Direct: https://github.com/periander/PSArcTool/raw/master/PSArcTool.zip
+This is a fork of https://github.com/periander/PSArcTool.
 Just drag PAK files on to PSArcTool.EXE to extract.
 Drag other file(s)/folder(s) on to create a PAK file.
 It's not real intelligent, but hopefully helps.
